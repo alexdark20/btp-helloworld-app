d7fbaf92b48238078e7b094dbc5698a83432308e
Oleksii Ryzhenko <ryzhenkoalex@gmail.com>
2024-11-11 17:18:50 +0000

    commit

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,13 +1,18 @@
 sap.ui.define([
     "sap/m/MessageBox",
 	"sap/m/MessageToast",
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
 ],
-function (MessageBox, MessageToast, Controller) {
+function (MessageBox, MessageToast, Controller, JSONModel) {
     "use strict";
 
     return Controller.extend("sap.btp.helloworldui5.controller.View1", {
         onInit: function () {
+            var oJsonModel = new JSONModel({
+				Shutterstock: sap.ui.require.toUrl("helloworldui5/images/283930_shutterstock_56287057.jpg"),
+			});
+			this.getView().setModel(oJsonModel);
         },
 
 		onPress: function (evt) {
